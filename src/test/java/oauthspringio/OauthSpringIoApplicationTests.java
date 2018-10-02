77df56538f78ff7bd263f382696063f0433aee7b
pjbielec <przemyslaw.bielec@core3.pl>
2018-10-02 13:38:03 +0200

    Initial commit

---
+++ src/test/java/oauthspringio/OauthSpringIoApplicationTests.java
@@ -0,0 +1,16 @@
+package oauthspringio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthSpringIoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
